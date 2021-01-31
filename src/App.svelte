37f6f7cd6914487485d818795eedd42da605fbff
samuba <samuel.z.bach@gmail.com>
2021-01-31 19:19:24 +0100

    started implementation

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,60 @@
 <script lang="ts">
-    import {  startTime, deleteTime, theTimes } from "./store";
+  import { addTime, deleteTime, theTimes, currentTime } from "./store";
+  import { format, formatDistanceToNowStrict, formatDistanceStrict } from "date-fns";
+  import { onDestroy } from "svelte";
 
-let name = "worldsssisss";
+  let since = "";
 
-function start() {
-  startTime();
-}
+  const interval = setInterval(() => {
+    if (!$currentTime.start) return since = "";
+    since = formatDistanceToNowStrict($currentTime.start);
+  }, 1000);
+  onDestroy(() => clearInterval(interval));
+
+  function formatPastTime(time) {
+    if (!time.start) return
+    if (!time.end) return "no end"
+    return `${formatDistanceStrict(time.start, time.end)} (${format(time.start, "HH:mm")} - ${time.end && format(time.end, "HH:mm")})`
+  }
 </script>
 
-<h1>Hello {name}!</h1>
+<div class="flex justify-center pt-8">
+  <div class="max-w-md">
+    <h1 class="text-2xl">Time-Tracker</h1>
 
-<button class="border-gray-300 border" on:click={startTime}>Start</button>
+    {#if !$currentTime.start}
+      <button
+        class="border-gray-300 border rounded p-2"
+        on:click={currentTime.start}
+      >
+        Start Timer
+      </button>
+    {:else}
+      <p>{since} (since {format($currentTime.start, "HH:mm")})</p>
+      <button
+        class="border-gray-300 border rounded p-2"
+        on:click={currentTime.stop}
+      >
+        Stop Timer
+      </button>
+    {/if}
 
-  {#each $theTimes as time}
-  <div>
-    <p>start: {time.start}</p>
-    <p>end: {time.end}</p>
-    
-    <button class="border-gray-300 border" on:click="{() => deleteTime(time.id)}">delete</button>
-    <br />
+    <div class="mt-10">
+      <h2 class="text-lg">All times</h2>
+
+      {#each $theTimes as time}
+        <div class="bg-gray-100 p-2 px-4 mb-4">
+          <span>{formatPastTime(time)}</span>
+          <button
+            class="border-gray-300 border rounded p-2"
+            on:click={() => deleteTime(time.id)}
+          >
+            delete
+          </button>
+        </div>
+      {:else}
+        ...loading data
+      {/each}
+    </div>
   </div>
-
-  {:else}
-    ...loading data 
-  {/each}
+</div>
--- src/store.ts
+++ src/store.ts
@@ -1,63 +1,72 @@
+import { formatDistance } from "date-fns";
 import firebase from "firebase"
 import "firebase/firestore"
-import { writable } from 'svelte/store';
+import { derived, readable, writable } from 'svelte/store';
 
-firebase.initializeApp({
-    apiKey: "AIzaSyAkkqMU1qe8HoCrEObNJXvxT3wIywz2ulU",
-    authDomain: "time-tracker-1af57.firebaseapp.com",
-    projectId: "time-tracker-1af57",
-    storageBucket: "time-tracker-1af57.appspot.com",
-    messagingSenderId: "191631527516",
-    appId: "1:191631527516:web:74b335167125e9e48de099"
-});
- 
-
+if (!firebase.apps.length) {
+    firebase.initializeApp({
+        apiKey: "AIzaSyAkkqMU1qe8HoCrEObNJXvxT3wIywz2ulU",
+        authDomain: "time-tracker-1af57.firebaseapp.com",
+        projectId: "time-tracker-1af57",
+        storageBucket: "time-tracker-1af57.appspot.com",
+        messagingSenderId: "191631527516",
+        appId: "1:191631527516:web:74b335167125e9e48de099"
+    });
+}
 const db = firebase.firestore()
 const times = db.collection("times")
 
-export const loading = writable(true);
+
+export type Time = {
+    id: string,
+    start: Date,
+    end: Date,
+}
+export type NewTime = Omit<Time, "id">
+
+function extractData(snapshot) {
+    const data = snapshot.data()
+    data.start = data.start ? data.start.toDate() : null
+    data.end = data.end ? data.end.toDate() : null
+    return { id: snapshot.id, ...data } as Time
+}
+
+
+// todo: get currentTime from store if already exists
+function createCurrentTime() {
+    const { subscribe, set, update } = writable({ start: null, end: null } as Time);
+    return {
+        subscribe,
+        start: async () => {
+            set(await addTime({ start: new Date(), end: null }))
+        },
+        stop: () => update(n => {
+            const end = new Date();
+            times.doc(n.id).update({ end })
+            return { start: null, end: null } as Time;
+        })
+    }
+}
+export const currentTime = createCurrentTime();
+
 
 function createTimes() {
-    const { subscribe, set, update } = writable([]);
-
+    const { subscribe, set, update } = writable([] as Time[]);
     times.onSnapshot(ss => {
-        const times = ss.docs.map(x => { return {  id: x.id, ...x.data() }})
+        const times = ss.docs.map(x => extractData(x))
         set(times);
-        loading.set(false)
     })
-    return {subscribe };
+    return { subscribe };
 }
-
 export const theTimes = createTimes()
 
-
-function createCount() {
-	const { subscribe, set, update } = writable(0);
-	return {
-		subscribe,
-		increment: () => update(n => n + 1),
-		decrement: () => update(n => n - 1),
-		reset: () => set(0)
-	};
-}
-
-export const count = createCount();
-
-export function getTimes() {
-    return times.get().then(snapshot => {
-        return snapshot.docs.map(x => {
-            return { ...x.data(), id: x.id }
-        })
+export function addTime(time: NewTime) {
+    return times.add(time).then(x => {
+        return { ...time, id: x.id } as Time
     })
 }
 
-export function startTime() {
-    times.add({
-        start: firebase.firestore.FieldValue.serverTimestamp(),
-        end: null
-    })
-}
-
-export function deleteTime(id) {
+export function deleteTime(id: string) {
+    console.log("delete time: " + id)
     return times.doc(id).delete();
 }
